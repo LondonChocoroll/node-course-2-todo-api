5d07fc1762637a4c0cd3e1861bd5c5323cf4e936
LondonChocoroll <jameswongwy@gmail.com>
2018-09-28 17:37:16 +1000

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser'); //convert JSON into an object
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
 var {User} = require('./models/user');
@@ -27,6 +28,30 @@ app.get('/todos', (req, res)=>{
   }, (e)=>{
     res.status(400).send(e);
   });
+});
+
+//GET /todos/1234324
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  //Validate id using isValid
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+   //404 = send back empty body
+   //findById
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(404).send();
+  });
+    //success
+      //if todo - send it back
+      //if no todo - send back 404 with empty 404
+    //error
+      //400 - send back nothing
 });
 
 
