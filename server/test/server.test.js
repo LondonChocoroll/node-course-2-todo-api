d691062ea5405c5135e5788a97ec649b0a6d395d
LondonChocoroll <jameswongwy@gmail.com>
2018-10-26 23:33:31 +1100

    expect update changes

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -130,7 +130,7 @@ describe('DELETE /todos/:id', ()=>{
 
         //query database using findById
         Todo.findById(hexId).then((todo)=>{
-          expect(todo).toNotExist();
+          expect(todo).toBeFalsy();
           done();
         }).catch((e)=> done(e));
       });
@@ -150,7 +150,7 @@ describe('DELETE /todos/:id', ()=>{
 
         //query database using findById
         Todo.findById(hexId).then((todo)=>{
-          expect(todo).toExist();
+          expect(todo).toBeTruthy();
           done();
         }).catch((e)=> done(e));
       });
@@ -191,7 +191,8 @@ describe('DELETE /todos/:id', ()=>{
         .expect((res)=>{
           expect(res.body.todo.text).toBe(text);
           expect(res.body.todo.completed).toBe(true);
-          expect(res.body.todo.completedAt).toBeA('number');
+          //expect(res.body.todo.completedAt).toBeA('number');
+          expect(typeof res.body.todo.completedAt).toBe('number');
         })
         .end(done)
     });
@@ -227,7 +228,7 @@ describe('DELETE /todos/:id', ()=>{
         .expect((res)=>{
           expect(res.body.todo.text).toBe(text);
           expect(res.body.todo.completed).toBe(false);
-          expect(res.body.todo.completedAt).toNotExist();
+          expect(res.body.todo.completedAt).toBeFalsy();
         })
         .end(done)
       //200
@@ -268,8 +269,8 @@ describe('DELETE /todos/:id', ()=>{
       .send({email, password})
       .expect(200)
       .expect((res)=>{
-        expect(res.headers['x-auth']).toExist();
-        expect(res.body._id).toExist();
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
       .end((err)=>{
@@ -278,8 +279,8 @@ describe('DELETE /todos/:id', ()=>{
         }
 
         User.findOne({email}).then((user)=>{
-          expect(user).toExist();
-          expect(user.password).toNotBe(password);
+          expect(user).toBeTruthy();
+          expect(user.password).not.toBe(password);
           done();
         }).catch((e)=>done(e));
       });
@@ -320,7 +321,7 @@ describe('DELETE /todos/:id', ()=>{
       })
       .expect(200)
       .expect((res)=>{
-        expect(res.header['x-auth']).toExist();
+        expect(res.header['x-auth']).toBeTruthy();
       })
       .end((err,res)=>{
         if(err){
@@ -328,7 +329,7 @@ describe('DELETE /todos/:id', ()=>{
         }
 
         User.findById(users[1]._id).then((user)=>{
-          expect(user.tokens[1]).toInclude({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: 'auth',
             token: res.header['x-auth']
           });
@@ -346,7 +347,7 @@ describe('DELETE /todos/:id', ()=>{
       })
       .expect(400)
       .expect((res)=>{
-        expect(res.header['x-auth']).toNotExist();
+        expect(res.header['x-auth']).toBeFalsy();
       })
       .end((err,res)=>{
         if(err){
